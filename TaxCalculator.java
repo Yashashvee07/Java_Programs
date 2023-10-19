ac266d4c4ad6231aa856015d1c325ac48a3cb23e
Yashashvee07 <147467028+Yashashvee07@users.noreply.github.com>
2023-10-18 20:26:45 -0700

    Add files via upload

--- TaxCalculator.java
+++ TaxCalculator.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
-public class TaxCalculator {
-    static int calculateTax(int salary){
-        int tax=0;
-        if(salary<=20000)
-        return tax;
-    }
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the salary amount");
-        int salary=sc.nextInt();
 
-        sc.close();
+public class TaxCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your income: ");
+        double income = scanner.nextDouble();
+
+        double tax = 0.0;
+
+        if (income < 10000) {
+            tax = 0.0;
+        } else if (income >= 10000 && income < 20000) {
+
+            tax = 0.10 * (income - 10000);
+        } else if (income >= 20000 && income < 30000) {
+
+            tax = (0.10 * 10000) + 0.15 * (income - 20000);
+        } else {
+
+            tax = (0.10 * 10000) + (0.15 * 10000) + 0.25 * (income - 30000);
+        }
+
+        System.out.println("Your income: $" + income);
+        System.out.println("Income tax: $" + tax);
+        
+        scanner.close();
     }
 }
